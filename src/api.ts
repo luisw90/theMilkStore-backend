632343d12c13189b2a1fe8c41dc1ed9ed4e539ea
luis barri <luisbarriw@gmail.com>
2023-06-02 15:59:04 +0200

    Adjusted api path

--- src/api.ts
+++ src/api.ts
@@ -18,7 +18,7 @@ router.get("/milk", async (req, res) => {
   return res.status(200).json({ results });
 });
 
-router.get("/milk:id", async (req, res) => {
+router.get("/milk/:id", async (req, res) => {
   const id = req.params["id"];
   if (id) {
     const milkProduct = results.filter((product) => product.id === id);
